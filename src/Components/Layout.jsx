28caecff59561be6bde0226bf34fbc567c33ba80
Jayraj Rathod <jayrahod5@gmail.com>
2022-09-05 19:14:50 +0530

    feat : Added customised theme using material-ui

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import * as React from 'react';
+import Navbar from './Navbar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+import Button from '@mui/material/Button';
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#6F64EF',
+		},
+		secondary: {
+			main: '#ff906e',
+		},
+		info: {
+			main: '#484554',
+		},
+	},
+});
 
 const Layout = () => {
-	return <div>Layout</div>;
+	return (
+		<div style={{ background: '#F8F7FD', height: '100vh' }}>
+			<ThemeProvider theme={theme}>
+				<Navbar />
+			</ThemeProvider>
+		</div>
+	);
 };
 
 export default Layout;
